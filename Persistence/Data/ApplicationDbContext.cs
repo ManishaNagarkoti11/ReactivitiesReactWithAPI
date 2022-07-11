7f6f9e180ab23ce6ba2ff6f8b7d5f2a6e3ae8c67
Manisha Nagarkoti <Manisha.nagarkoti40@outlook.com>
2022-07-11 11:23:51 +0545

    Add project files.

---
+++ Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
